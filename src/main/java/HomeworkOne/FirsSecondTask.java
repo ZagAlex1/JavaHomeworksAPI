0293fec5128d250c429c7b680efd68d7f7777cc1
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-06-10 13:59:21 +0300

    Initial commit

---
+++ src/main/java/HomeworkOne/FirsSecondTask.java
@@ -0,0 +1,14 @@
+package HomeworkOne;
+
+//1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
+//2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+
+public class FirsSecondTask {
+
+    public static void main(String[] args) {
+
+    }
+
+    
+}
+
